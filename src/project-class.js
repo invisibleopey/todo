78a2d796c810ce4b29b9ceb9083eb084c58aafc0
Abdullahi Muritala <oabdullahi37@gmail.com>
2021-08-24 03:49:20 +0100

    Added a module to handle creation of new projects obj

---
+++ src/project-class.js
@@ -0,0 +1,12 @@
+class Projects {
+  constructor(title) {
+    this.title = title;
+    this.myArray = [];
+  }
+  renderProject() {
+    // Code to render content of this.myArray
+  }
+  // Add logic to remove the projects once all the tasks added are deleted
+}
+
+export default Projects;
