15741660911aa123deeca1809bb9b37e5ad79469
Abdullahi Muritala <oabdullahi37@gmail.com>
2021-08-30 12:10:19 +0100

    Added a sortProject function, sort tasks into proj

--- src/project-class.js
+++ src/project-class.js
@@ -8,7 +8,6 @@ class Projects {
   }
   renderProjectTasks() {
     document.querySelector("#container").innerHTML = "";
-    if (this.Array === undefined) return;
     this.myArray.map(render);
     // Code to render content of this.myArray
   }
@@ -21,16 +20,33 @@ allProjects.map(renderProjects);
 // Use event listener to call projects class to create new array
 const addProjectBtn = document.querySelector("#addProject");
 addProjectBtn.addEventListener("click", addNewProject);
-function addNewProject(e) {
-  e.preventDefault();
-  let title = document.querySelector("#new-project-title").value;
-  let newProject = new Projects(title);
-  allProjects.push(newProject);
+function addNewProject() {
+  createProject();
   // display projects and hide modals
   document.querySelector("#project-list").innerHTML = "";
   allProjects.map(renderProjects);
   document.querySelector(".bg-modal").style.display = "none";
   document.querySelector("#new-project-form").reset();
 }
-
+function createProject() {
+  // Used this to get value of title either I am creating a new project from
+  // project form or from task form.
+  let title =
+    document.querySelector("#new-project-title").value ||
+    document.querySelector("#project-name").value;
+  let newProject = new Projects(title);
+  allProjects.push(newProject);
+}
+// TODO: Not working, Fix it
+export function sortProject(newTask) {
+  const projectName = document.querySelector("#project-name").value;
+  if (projectName === "") return;
+  for (let i = 0; i < allProjects.length; i++) {
+    if (allProjects[i].title === projectName) {
+      allProjects[i].myArray.push(newTask);
+    } else {
+      addNewProject();
+    }
+  }
+}
 export default Projects;
