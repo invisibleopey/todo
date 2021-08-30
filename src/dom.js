171b8acc96934fefc03036dcad6107df214a04da
Abdullahi Muritala <oabdullahi37@gmail.com>
2021-08-31 00:20:52 +0100

    Added event handler for each projects

--- src/dom.js
+++ src/dom.js
@@ -1,4 +1,5 @@
 import { restoreLocal } from "./index.js";
+import { allProjects } from "./project-class.js";
 const container = document.querySelector("#container");
 function render(obj) {
   // Create the Div and its contents
@@ -51,14 +52,29 @@ export function renderProjects(obj) {
   let li = document.createElement("li");
   let span = document.createElement("span");
   span.textContent = obj.title;
+  span.classList.add("project");
   li.append(span);
   ul.append(li);
-  span.addEventListener("click", obj.renderProjectTasks.bind(obj));
+  // span.addEventListener("click", obj.renderProjectTasks.bind(obj));
 }
 // Event listener for home button
 const homeBtn = document.querySelector("#home");
 homeBtn.addEventListener("click", () => {
-  document.querySelector("#container").innerHTML = "";
   restoreLocal();
 });
+// Event Listener to render the array in each project.
+// Method of class Project was giving me problem so I moved it here.
+const projectList = document.querySelectorAll(".project");
+projectList.forEach((element) =>
+  element.addEventListener("click", addEventToProject)
+);
+function addEventToProject(e) {
+  document.querySelector("#container").innerHTML = "";
+  let theProject = e.target.outerText.toLowerCase();
+  for (let project of allProjects) {
+    if (project.title.toLowerCase() === theProject) {
+      project.myArray.map(render);
+    }
+  }
+}
 export default render;
