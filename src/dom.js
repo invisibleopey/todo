6eca055c7e1be563c38a6263abd46d175bd2455a
Abdullahi Muritala <oabdullahi37@gmail.com>
2021-09-05 00:29:24 +0100

    Added event listener to editBtn, with renderEditForm and submitEdit functions

--- src/dom.js
+++ src/dom.js
@@ -27,6 +27,7 @@ function render(obj) {
   let editBtn = document.createElement("button");
   editBtn.textContent = "EDIT";
   editBtn.classList.add("task-btns");
+  editBtn.id = "editBtn";
   let delBtn = document.createElement("button");
   delBtn.textContent = "X";
   delBtn.classList.add("task-btns");
@@ -103,9 +104,7 @@ function deleteTask(e) {
     }
   }
 }
-// TODO: Event Listener for editBtn and detailsBtn
 // Event Listener for the checkBox for marking done
-// TODO: Found a bug here, fix it
 container.addEventListener("click", toggleDone);
 function toggleDone(e) {
   if (e.target.id !== "toggleDoneBox") return;
@@ -213,5 +212,168 @@ function renderDetailsPopUp(title, project, priority, dueDate, details) {
   );
   bgModal.append(modalContents);
   body.append(bgModal);
+}
+// Event Listener for editBtn
+let edittedTaskIndex;
+container.addEventListener("click", editTask);
+function editTask(e) {
+  if (e.target.id !== "editBtn") return;
+  let targetTitle = e.target.parentNode.children[1].outerText;
+  for (let i = 0; i < allProjects[0].myArray.length; i++) {
+    if (targetTitle === allProjects[0].myArray[i].title) {
+      edittedTaskIndex = i; // saved value of current iterated attribute for editing later
+      let targetTask = allProjects[0].myArray[i];
+      let title = targetTask.title;
+      let priority = targetTask.priority;
+      let dueDate = targetTask.dueDate;
+      let details = targetTask.description;
+      renderEditForm(title, priority, dueDate, details);
+    }
+  }
+}
+// Render Edit Form Function
+function renderEditForm(title, priority, dueDate, details) {
+  let body = document.querySelector("body");
+  //Remove the DOM created edit form
+  if (document.contains(document.querySelector(".edit-bg-modal"))) {
+    document.querySelector(".edit-bg-modal").remove();
+  }
+  // Create div for modal bg and modal content
+  let bgModal = document.createElement("div");
+  bgModal.classList.add("bg-modal");
+  bgModal.style.display = "flex";
+  bgModal.classList.add("edit-bg-modal");
+  let modalContents = document.createElement("div");
+  modalContents.classList.add("modal-contents");
+  modalContents.classList.add("edit-form-pop-up"); // TODO: Change this to form class
+  // Start Creating the details within the Edit container
+  // Close
+  let close = document.createElement("div");
+  close.textContent = "+";
+  close.classList.add("close");
+  close.addEventListener("click", function () {
+    bgModal.style.display = "none";
+  });
+  // Form
+  let form = document.createElement("form");
+  form.action = "#";
+  form.classList.add("edit-task-form");
+  // Title
+  let titleInput = document.createElement("input");
+  titleInput.type = "text";
+  titleInput.name = "title";
+  titleInput.classList.add("edit-task-title");
+  titleInput.value = title;
+  titleInput.required = true;
+  // Details
+  let detailsInput = document.createElement("input");
+  detailsInput.type = "text";
+  detailsInput.name = "details";
+  detailsInput.classList.add("edit-task-details");
+  detailsInput.value = details;
+  detailsInput.required = true;
+  // Labels for Due Date
+  let dueDateLabel = document.createElement("label");
+  dueDateLabel.for = "due-date";
+  // DueDate
+  let dueDateInput = document.createElement("input");
+  dueDateInput.type = "date";
+  dueDateInput.name = "due-date";
+  dueDateInput.classList.add("edit-task-due-date");
+  dueDateInput.value = dueDate;
+  dueDateInput.required = true;
+  // Labels for Priority
+  let priorityLabel = document.createElement("label");
+  priorityLabel.for = "priority";
+  // Radio for Priority Low
+  let lowPriorityRadio = document.createElement("input");
+  lowPriorityRadio.type = "radio";
+  lowPriorityRadio.name = "edit-priority";
+  lowPriorityRadio.classList.add("edit-task-low");
+  lowPriorityRadio.value = "low";
+  // Radio for Priority Medium
+  let mediumPriorityRadio = document.createElement("input");
+  mediumPriorityRadio.type = "radio";
+  mediumPriorityRadio.name = "edit-priority";
+  mediumPriorityRadio.classList.add("edit-task-medium");
+  mediumPriorityRadio.value = "medium";
+  // Radio for Priority High
+  let highPriorityRadio = document.createElement("input");
+  highPriorityRadio.type = "radio";
+  highPriorityRadio.name = "edit-priority";
+  highPriorityRadio.classList.add("edit-task-high");
+  highPriorityRadio.value = "high";
+  // Logic to specify which radio is checked
+  switch (priority) {
+    case lowPriorityRadio.value:
+      lowPriorityRadio.checked = true;
+      break;
+    case mediumPriorityRadio.value:
+      mediumPriorityRadio.checked = true;
+      break;
+    case highPriorityRadio.value:
+      highPriorityRadio.checked = true;
+  }
+  // Confirm Edit Button
+  let confirmEditBtn = document.createElement("input");
+  confirmEditBtn.type = "submit";
+  confirmEditBtn.value = "CONFIRM EDIT";
+  confirmEditBtn.classList.add("confirmEditBtn");
+  // Append all to form
+  form.append(
+    titleInput,
+    detailsInput,
+    dueDateLabel,
+    dueDateInput,
+    priorityLabel,
+    lowPriorityRadio,
+    mediumPriorityRadio,
+    highPriorityRadio,
+    confirmEditBtn
+  );
+  // Append close and form to modal content
+  modalContents.append(close, form);
+  bgModal.append(modalContents);
+  body.append(bgModal);
+  // Add Event Listener to confirmEditBtn
+  confirmEditBtn.addEventListener("click", confirmEdit);
+}
+// Event Listener for Confirm Edit Btn
+function confirmEdit(e) {
+  e.preventDefault();
+  let newTitle = document.querySelector(".edit-task-title").value;
+  let newDescription = document.querySelector(".edit-task-details").value;
+  let newDueDate = document.querySelector(".edit-task-due-date").value;
+  let newPriority = document.querySelector(
+    'input[name="edit-priority"]:checked'
+  ).value;
+  let edittedTask = allProjects[0].myArray[edittedTaskIndex];
+  // Call a method that set the right details using the above as parameters/ argumets
+  setEdittedTask(
+    edittedTask,
+    newTitle,
+    newDescription,
+    newDueDate,
+    newPriority
+  );
+  saveLocalProjects();
+  // The next line of code was added because the contents of the homeArray kept duplicating during rendering. A temporary fix, find permanent fix later
+  document.querySelector("#container").innerHTML = "";
+  allProjects[0].myArray.map(render);
+  document.querySelector(".edit-bg-modal").style.display = "none";
+  document.querySelector(".edit-task-form").reset();
+}
+// Function to set the new details into the target object
+function setEdittedTask(
+  edittedTask,
+  newTitle,
+  newDescription,
+  newDueDate,
+  newPriority
+) {
+  edittedTask.title = newTitle;
+  edittedTask.description = newDescription;
+  edittedTask.dueDate = newDueDate;
+  edittedTask.priority = newPriority;
 }
 export default render;
