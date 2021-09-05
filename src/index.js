783d921eeaf0e16db95f71db15bbea1b05f63082
Abdullahi Muritala <oabdullahi37@gmail.com>
2021-09-06 00:59:16 +0100

    Added prompt for username on first visit and welcome message.

--- src/index.js
+++ src/index.js
@@ -2,5 +2,12 @@ import Task from "./task.js";
 import render from "./dom.js";
 import { allProjects } from "./project-class.js";
 
+let userName = localStorage.getItem("userName");
+if (!userName || userName === null) {
+  userName = prompt("Welcome, please enter your name...");
+  localStorage.setItem("userName", userName);
+}
+let welcomeMessage = document.querySelector("#welcome-msg");
+welcomeMessage.textContent = `Welcome back, ${userName}`;
 // Call this function everytime my app is revisited or reloaded
 allProjects[0].myArray.map(render);
